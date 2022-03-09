ac7196811648b9421cd2d95e1246720a552cbb21
Vaibhavx10 <34233078+Vaibhavx10@users.noreply.github.com>
2022-03-09 16:26:50 +0000

    added heroku stuff

--- webMovieApp/webMovieApp/settings.py
+++ webMovieApp/webMovieApp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4a5bl342^0&11xk-vu!n(z)@f0^jiiqa9^ls_n$nr24t#n9dnu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sem2webvideoapp.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'webMovieApp.urls'
