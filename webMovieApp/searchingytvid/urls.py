99f24d640ef5546c8b3bebede96a7340ccd8d688
Vaibhavx10 <34233078+Vaibhavx10@users.noreply.github.com>
2022-03-09 15:59:29 +0000

    All Major Changes done

--- webMovieApp/searchingytvid/urls.py
+++ webMovieApp/searchingytvid/urls.py
@@ -3,14 +3,16 @@ from . import views
 from .views import addSubscriptionInDB
 from .views import getUserSubscribedData
 from .views import deleteVideoEntry
+from .views import displatytsearchresultfor_home
 
 urlpatterns = [
     path('',views.returnYTSearchPage,name='index'),
-    path('displatytsearchresult',views.displayHome,name='displatytsearchresult'),
+    path('displatytsearchresultfor_home',displatytsearchresultfor_home.as_view()),
     path('dashboard',views.callDashoardPage,name='dashboard'),
     path('moreInfo',views.moreInfo,name='moreInfo'),
     path('addSubscriptionInDB',addSubscriptionInDB.as_view()),
     path('getUserSubscribedData',getUserSubscribedData.as_view()),
     path('deleteVideoEntry',deleteVideoEntry.as_view()),
     path('getDashboardPageonUserID',views.getDashboardPageonUserID,name='getDashboardPageonUserID'),
+    path('displatytsearchresult',views.displatytsearchresult,name='displatytsearchresult'),
 ]
